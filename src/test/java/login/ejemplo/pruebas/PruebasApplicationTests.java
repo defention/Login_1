ce4ba9c0b5a3b0d551c07082ec2e6fda72134da1
Defention <oscar-rdz-15@hotmail.com>
2022-08-10 22:49:34 -0500

    Subida de Proyecto

---
+++ src/test/java/login/ejemplo/pruebas/PruebasApplicationTests.java
@@ -0,0 +1,13 @@
+package login.ejemplo.pruebas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
